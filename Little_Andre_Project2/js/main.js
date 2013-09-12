40621950010becff7a1f1a8df687cad6aaabcc7a
Andre Little <ALittle@Fullsail.edu>
2013-09-12 16:10:03 -0500

    Started my JS file.

---
+++ Little_Andre_Project2/js/main.js
@@ -0,0 +1,69 @@
+/*
+Andre Little
+Class: Visual Frame Works
+Term: 1309
+Instructor: Mr. Gibson
+Completed Date: 9/12/2013
+Project 2: Mega Weight Lost Project - JS Source
+*/
+
+// The beginning of the Dom.
+window.addEventListener("DOMContentLoaded", function(){
+	
+	/* ------ Shortcuts ------ */
+	// This will get the Element by Id.
+	var getId = function (x){
+		var elId = document.getElementById(x);
+		return elId;
+	};
+	
+	var getTag = function (x){
+		var elTag = document.getElementsByTagName(x);
+		return elTag;
+	};
+	
+	var createEl = function (){
+		var makeEl = document.createElement(x);
+		return makeEl;
+	}
+	/* ------ End of the Shortcuts ------ */
+	
+	// Create drop down.
+
+	
+	
+	
+	
+	// Default Variables
+	var mealTimes = ["--Make a Choice--", "Breakfast", "Lunch", "Dinner"];
+	
+	
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//Links and Click Submit Events.
+	var displayData =
+	displayData.addEventListener("click", gotData);
+	var clearData =	
+	clearData.addEventListener("click", dataCleared);
+	var saveData = 
+	saveData.addEventListener("click", dataSaved);
+	
+
+});
