c8c752892335d1ca62a1913cf0d7e327ae5b8ea2
Andre Little <ALittle@Fullsail.edu>
2013-09-26 03:36:11 -0500

    Added my JSON Data Source to Project 4

---
+++ Little_Andre_Project4/js/JSON.js
@@ -0,0 +1,23 @@
+/*
+Andre Little
+Class: Visual Frame Works
+Term: 1309
+Instructor: Mr. Gibson
+Completed Date: 9/26/2013
+Project 4: Mega Weight Lost Project - JSON DATA Source
+*/
+
+var json = {
+	"testMeal1": {
+		"dates": ["Date:", "2013-09-25"],
+		"times": ["Meal Time:", "Breakfast"],
+		"itemname": ["Name:", "Sausage"],
+		"fatgrams": ["Grams of Fat:", "2.5"],
+		"carbgrams": ["Grams of Carbs:", "5"],
+		"programs": ["Grams of Protein:", ".5"],
+		"size": ["Serving Size:", "1"],
+		"additional": ["Add Item:", "No"],
+		"notes": ["Notes about Meal:", "Turkey Linked" ]
+	}
+	
+}
